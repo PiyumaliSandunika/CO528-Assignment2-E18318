ab2929e763193fd2d6fc300505d589aecac9c5d3
PiyumaliSandunika <e18318@eng.pdn.ac.lk>
2024-08-31 08:33:30 +0530

    handle negative values

--- src/components/AngleInput.js
+++ src/components/AngleInput.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
 const AngleInput = ({ angle, onChange }) => {
+    const normalizeAngle = (angle) => {
+        return (angle % 360 + 360) % 360;
+    };
+
     return (
         <div className="input-group">
             <label htmlFor="angleInput">Angle: </label>
             <input
                 type="number"
                 id="angleInput"
-                value={angle}
+                value={normalizeAngle(angle)}
                 min="0"
                 max="360"
-                onChange={(e) => onChange(parseInt((e.target.value)%360))}
+                onChange={(e) => onChange(normalizeAngle(parseInt(e.target.value)))}
             />
         </div>
     );
